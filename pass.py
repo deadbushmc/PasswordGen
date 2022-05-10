6d6092824be953377a05ca49fe5cda2c3a5f4248
DeadBush <66917109+deadbushmc@users.noreply.github.com>
2022-05-10 14:56:24 +0530

    Update pass.py

--- pass.py
+++ pass.py
@@ -1,8 +1,34 @@
 import random
 import string
+import time
 
-length=int(input("Enter the password legth: "))
+t = 1
+def intro():
+    print("Welcome to password gen")
+    time.sleep(t)
+    print("By DeadBush")
+    time.sleep(t)
+    gen()
 
-password = ''.join(random.choices(string.ascii_letters + string.punctuation + string.digits, k = length))
+def gen():
+    length=int(input("Enter the password legth: "))
+    print("Generating Your Password Please Wait")
+    time.sleep(3)
+    password = ''.join(random.choices(string.ascii_letters + string.punctuation + string.digits, k = length))
+    print(f"Your password is: {password}")
+    time.sleep(t)
+    cb=input("Do you want to continue? [y/n]: ")
+    cont(cb)
 
-print(f"Your password is: {password}")
+def cont(cb):
+    if cb.lower() == "y":
+        intro()
+    elif cb.lower() == "n":
+        outro()
+    else:
+        print(f"{cb} is not a valid option")
+
+def outro():
+    print("Thank you for using password gen by DeadBush!")
+
+intro()
